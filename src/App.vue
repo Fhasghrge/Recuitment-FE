4abe038b788fed937388565636437569ddbf7dd6
XL T <xlt@XLdeMacBook-Pro.local>
2019-07-12 22:50:37 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-
+body {
+  background-image: url(../src/assets/back.png);
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  margin: 0;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './pages/login/home.vue'
+import answer from './pages/answer/answer.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [{
+    path: '/home',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/answer',
+    name: 'answer',
+    component: answer
+  }
+  ]
 })
