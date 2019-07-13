c5690201fbf7fa967e48c7a0e59cb30e7a025fda
MrThanlon <i@anclx.cc>
2019-07-13 10:58:38 +0800

    feat: background image position:fixed
    Signed-off-by: MrThanlon <i@anclx.cc>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="app">
+    <div class="background">
+    </div>
     <router-view />
   </div>
 </template>
 
 <style>
 body {
-  background-image: url(../src/assets/back.png);
+  /*background-image: url(../src/assets/back.png);*/
   background-repeat: no-repeat;
   background-size: 100% 100%;
   margin: 0;
 }
+  .background {
+    background-image: url(../src/assets/back.png);
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+    z-index: -100;
+  }
 </style>
