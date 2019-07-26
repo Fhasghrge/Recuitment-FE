85528633adcd5be1fe86549c7bab33c7b5ec07fa
unknown <1398758415@qq.com>
2019-07-26 09:15:53 +0800

    change API url
    Signed-off-by: unknown <1398758415@qq.com>

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import store from './store'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://121.48.165.58:17839'
+// axios.defaults.baseURL = 'http://121.48.165.58:17839'
+axios.defaults.baseURL = 'http://join.stuhome.com/api'
 axios.defaults.headers = {
   'Content-Type': 'application/x-www-form-urlencoded'
 }
